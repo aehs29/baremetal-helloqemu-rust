13672655a4a9532dbe3606bc873c75a819a6a7ce
Alejandro Hernandez Samaniego <alejandro@enedino.org>
2022-12-12 21:17:43 -0700

    baremetal-helloworld-rust: Add source code and submodule dependency
    Signed-off-by: Alejandro Hernandez Samaniego <alejandro@enedino.org>

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![no_main]
+
+use core::arch::global_asm;
+use core::ptr;
+
+mod panic;
+
+global_asm!(include_str!("baremetal-helloqemu/startup_aarch64.s"));
+
+#[no_mangle]
+pub extern "C" fn c_entry() {
+    const UART0: *mut u8 = 0x0900_0000 as *mut u8;
+    let welcome_str = b"Hello Rust from OpenEmbedded in Aarch64!\n";
+    for c in welcome_str {
+        unsafe {
+            ptr::write_volatile(UART0, *c);
+        }
+    }
+}
