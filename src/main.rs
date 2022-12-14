86b7d952736bb1fe0271d3c4322517622926c7a4
Alejandro Enedino Hernandez Samaniego <alejandro@enedino.org>
2022-12-13 22:08:33 -0700

    v2
    Signed-off-by: Alejandro Enedino Hernandez Samaniego <alejandro@enedino.org>

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use core::ptr;
 
 mod panic;
 
-global_asm!(include_str!("baremetal-helloqemu/startup_aarch64.s"));
+global_asm!(include_str!("startup_aarch64.s"));
 
 #[no_mangle]
-pub extern "C" fn c_entry() {
+pub extern "C" fn rust_entry() {
     const UART0: *mut u8 = 0x0900_0000 as *mut u8;
     let welcome_str = b"Hello Rust from OpenEmbedded in Aarch64!\n";
     for c in welcome_str {
